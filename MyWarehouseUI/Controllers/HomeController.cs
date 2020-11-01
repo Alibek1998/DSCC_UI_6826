d723e4bbd603419a6d2f8982a4d2c85f6802a01f
Alibek Shokhnazarov <39170097+Alibek1998@users.noreply.github.com>
2020-11-01 21:22:26 +0500

    Creating repository

---
+++ MyWarehouseUI/Controllers/HomeController.cs
@@ -0,0 +1,213 @@
+﻿using MyWarehouseUI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWarehouseUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Hosted web API REST Service base url
+        private string Baseurl = "http://ec2-54-145-253-253.compute-1.amazonaws.com/";
+
+
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            List<Product> ProdInfo = new List<Product>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("/products");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var PrResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Product list
+                    ProdInfo = JsonConvert.DeserializeObject<List<Product>>(PrResponse);
+                }
+                //returning the Product list to view
+                return View(ProdInfo);
+            }
+        }
+
+        // GET: Product/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            Product ProdInfo = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                HttpResponseMessage Res = await client.GetAsync("products/" + id);
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var PrResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Product list
+                    ProdInfo = JsonConvert.DeserializeObject<Product>(PrResponse);
+
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+            }
+            return View(ProdInfo);
+        }
+        // POST: Product/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, Product prod)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+                    HttpResponseMessage Res = await client.GetAsync("products/" + id);
+                    Product ProdInfo = null;
+                    //Checking the response is successful or not which is sent using HttpClient
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api
+                        var PrResponse = Res.Content.ReadAsStringAsync().Result;
+                        //Deserializing the response recieved from web api and storing into the Product list
+                        ProdInfo = JsonConvert.DeserializeObject<Product>(PrResponse);
+                    }
+                    //prod.CategoryId = ProdInfo.CategoryId;
+                    //HTTP PUT
+                    var postTask = client.PutAsJsonAsync<Product>("/products/" + prod.Id, prod);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                //ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                //return View(ProdInfo);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            Product ProdInfo = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                HttpResponseMessage Res = await client.GetAsync("products/" + id);
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var PrResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Product list
+                    ProdInfo = JsonConvert.DeserializeObject<Product>(PrResponse);
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+            }
+            return View(ProdInfo);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            
+            Product ProdInfo = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                HttpResponseMessage Res = await client.GetAsync("products/" + id);
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var PrResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Product list
+                    ProdInfo = JsonConvert.DeserializeObject<Product>(PrResponse);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                }
+                    
+            }
+            return View(ProdInfo);
+        }
+
+
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                HttpResponseMessage Res = await client.DeleteAsync("products/" + id);
+                return RedirectToAction("/");
+            }
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                string Baseurl = "http://localhost:45284/";
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    //HTTP POST
+                    var postTask = client.PostAsJsonAsync<Product>("/products", prod);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(result.StatusCode.ToString(), "Unable to create product!");
+                        return View(prod);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", ex.Message);
+                return View(prod);
+            }
+        }
+    }
+}
